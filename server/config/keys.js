49a2d613ee7542ecc0981ef361cfe11b27d04506
Juan <j.rodriguez@mediasciencelabs.com>
2018-06-11 12:55:51 -0500

    added google OAuth and mLabs to config/keys file

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://juanrod88:juanrod88@ds163681.mlab.com:63681/sorting-app",
+  googleClientID:
+    "43262550000-pklcvb0gj6r7p2eb48sic9gtpjtum10i.apps.googleusercontent.com",
+  googleClientSecret: "Hkyi4iXcR1Rgy0zvrdcCG5E2"
+};
