984cd576da3cfa84c459306bd141712aa0485144
Juan <j.rodriguez@mediasciencelabs.com>
2018-06-11 15:33:21 -0500

    moved production keys for google oauth and mlabs to heroku config vars

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://juanrod1:juanrod1@ds163681.mlab.com:63681/sorting-app",
-  googleClientID:
-    "43262550000-pklcvb0gj6r7p2eb48sic9gtpjtum10i.apps.googleusercontent.com",
-  googleClientSecret: "Hkyi4iXcR1Rgy0zvrdcCG5E2"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
